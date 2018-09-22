c744fc0fd049ca2013487ff6ca37ac9fe6c12640
Jonathan Andersson <ja1984@gmail.com>
2018-09-22 09:12:09 +0200

    Add some more field types

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -63,6 +63,7 @@ export default {
   top: 0;
   left: 0;
   right: 0;
+  z-index: 500;
 
   &.mini {
     span {
--- src/views/NewTemplate.vue
+++ src/views/NewTemplate.vue
@@ -1,44 +1,68 @@
 <template>
   <div class="new-template">
     <div class="container">
-      <div class="card">
-        <div class="card-header">
-          <span class="card-title">Create new template</span>
+      <div class="row">
+        <div class="column">
+          <main>
+            <div class="form-row">
+              <label>
+                Template name
+                <input type="text" v-model="name">
+              </label>
+            </div>
+            <div class="divider"></div>
+            <strong>Fields</strong>
+            <div class="form-row" v-for="field in fields" :key="field.id">
+              <div class="row row-center-vertically">
+                <div class="column"><input type="text" v-model="field.name" /></div>
+                <div class="column">{{field.type}}</div>
+              </div>
+            </div>
+          </main>
         </div>
-        <div class="card-body">
-          <div class="row">
-            <div class="column">
-              <main>
-                <div>
-                  <label>
-                    Template name
-                    <input type="text" v-model="name">
-                  </label>
-                </div>
-                <div class="form-row" v-for="field in fields" :key="field.id">
-                  <div class="row">
-                    <div class="column"><input type="text" v-model="field.name" /></div>
-                    <div class="column">{{field.type}}</div>
-                  </div>
-                </div>
-                <div class="toolbar">
-                  <button @click="createTemplate" class="button button-primary">Save template</button>
-                </div>
-              </main>
+        <div class="column column-wrap">
+          <aside>
+            <div class="new-field-toolbar">
+              <ul>
+                <li>
+                  <button class="toolbar-button" @click="addField('input')">
+                    <i class="fas fa-toggle-on fa-fw"></i> Text</button>
+                </li>
+                <li>
+                  <button class="toolbar-button" @click="addField('textarea')">
+                    <i class="fas fa-code fa-fw"></i> Textarea</button>
+                </li>
+                <li>
+                  <button class="toolbar-button" @click="addField('boolean')">
+                    <i class="fas fa-toggle-on fa-fw"></i> True / false</button>
+                </li>
+                <li>
+                  <button class="toolbar-button" @click="addField('number')">
+                    <i class="fas fa-sort-numeric-up"></i> Number</button>
+                </li>
+                <li>
+                  <button class="toolbar-button" @click="addField('date')">
+                    <i class="fas fa-calendar-alt fa-fw"></i> Date</button>
+                </li>
+                <li>
+                  <button class="toolbar-button" @click="addField('group')">
+                    <i class="far fa-folder fa-fw"></i> Group</button>
+                </li>
+                <li>
+                  <button class="toolbar-button" @click="addField('media')">
+                    <i class="far fa-image fa-fw"></i> Media</button>
+                </li>
+                <li></li>
+                <li></li>
+              </ul>
             </div>
-            <div class="column column-wrap">
-              <aside>
-                <div class="toolbar">
-                  <button @click="addNewTextbox">New textbox</button>
-                  <button @click="addNewTextarea">New textarea</button>
-                </div>
-              </aside>
+            <div class="toolbar">
+              <button @click="createTemplate" class="button button-success button-block">Save template</button>
             </div>
+          </aside>
+        </div>
 
-          </div>
-        </div>
       </div>
-
     </div>
   </div>
 </template>
@@ -54,18 +78,11 @@ export default {
     };
   },
   methods: {
-    addNewTextbox() {
+    addField(type) {
       this.fields.push({
         id: this.createId(),
         name: '',
-        type: 'input',
-      });
-    },
-    addNewTextarea() {
-      this.fields.push({
-        id: this.createId(),
-        name: '',
-        type: 'textarea',
+        type,
       });
     },
     createTemplate() {
@@ -83,5 +100,48 @@ export default {
 <style lang="less" scoped>
 .editor {
   display: flex;
+}
+
+aside {
+  width: 25rem;
+}
+
+li {
+  padding: 0.25rem 0;
+
+  i {
+    font-size: 2rem;
+    vertical-align: middle;
+    margin-right: 0.5rem;
+  }
+}
+
+.toolbar-button {
+  border: none;
+  padding: 1rem;
+  cursor: pointer;
+  transition: all ease 0.3s;
+  font-weight: 600;
+  font-size: 1.5rem;
+  display: block;
+  width: 100%;
+  text-align: left;
+  background: transparent;
+
+  &:active,
+  &:focus {
+    outline: none;
+  }
+
+  &:hover {
+    background: #fff;
+  }
+}
+
+.divider {
+  width: 100%;
+  height: 0.1rem;
+  background: #ccc;
+  margin-bottom: 1.5rem;
 }
 </style>
