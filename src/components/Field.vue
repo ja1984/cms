00a62c441cc147f0eb32a191909e402aabe038bf
Jonathan Andersson <ja1984@gmail.com>
2018-09-24 21:34:49 +0200

    Add date picker field

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -10,6 +10,7 @@
     <number-field v-model="data.value" :name="data.name" :required="data.required" v-if="data.type === 'number'"></number-field>
     <textarea-field v-model="data.value" :name="data.name" :required="data.required" v-if="data.type === 'textarea'"></textarea-field>
     <boolean-field v-model="data.value" :required="data.required" v-if="data.type === 'boolean'">{{data.name}}</boolean-field>
+    <date-picker-field v-model="data.value" :name="data.name" :required="data.required" v-if="data.type === 'date'"></date-picker-field>
   </div>
 </template>
 
@@ -19,6 +20,7 @@ import InputField from '@/components/fields/InputField.vue';
 import TextareaField from '@/components/fields/TextareaField.vue';
 import BooleanField from '@/components/fields/BooleanField.vue';
 import NumberField from '@/components/fields/NumberField.vue';
+import DatePickerField from '@/components/fields/DatePickerField.vue';
 
 export default {
   name: 'Field',
@@ -27,6 +29,7 @@ export default {
     TextareaField,
     BooleanField,
     NumberField,
+    DatePickerField,
   },
   props: {
     data: { type: Object },
---
+++ src/components/fields/DatePickerField.vue
@@ -0,0 +1,28 @@
+<template>
+  <label>
+    {{name}}
+    <datepicker :value="value" @selected="selected"></datepicker>
+  </label>
+</template>
+<script>
+import Datepicker from 'vuejs-datepicker';
+
+export default {
+  name: 'DatePickerField',
+  components: {
+    Datepicker,
+  },
+  props: {
+    value: { type: Date },
+    name: { type: String },
+  },
+  methods: {
+    selected(date) {
+      this.$emit('input', date);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+</style>
