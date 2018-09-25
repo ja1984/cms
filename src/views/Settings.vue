67682dc9997ebb56b204dc0f23368188385da3b7
Jonathan Andersson <ja1984@gmail.com>
2018-09-25 21:48:50 +0200

    :sparkles: Add settings page

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,25 +1,71 @@
 <template>
   <div class="container">
     <h1 class="page-title">Settings</h1>
-    <div class="card">
-      <div class="card-header row">
-        <div class="column">
-          <span class="card-title">Pages</span>
+
+    <ul class="tabs">
+      <li class="tab" v-for="tab in tabs" v-bind:key="tab" :class="{'active': tab === selectedTab}" @click="selectedTab = tab">{{tab}}</li>
+    </ul>
+
+    <section v-if="selectedTab === 'Localization'">
+      <form @submit.prevent="saveLanguage">
+        <div class="form-row">
+          <label>
+            <span>Language</span>
+            <input type="text" required placeholder="English" v-model="language">
+          </label>
         </div>
-        <div class="column column-wrap">
-          <router-link to="/pages/create" class="button button-primary">New page</router-link>
+        <div class="form-row">
+          <label>
+            <span>API Slug</span>
+            <input type="text" placeholder="en" required v-model="slug">
+          </label>
+        </div>
+        <div>
+          <button class="button button-success" type="submit">Add language</button>
+        </div>
+      </form>
+
+      <div class="site-languages">
+        <div class="site-language row" v-for="language in languages" :key="language.slug">
+          <div class="column">{{language.language}}</div>
+          <div class="column">{{language.slug}}</div>
+          <div class="column">{{language.isDefault}}</div>
+          <div class="column"><button @click="deleteLanguage(language.slug)"><i class="far fa-trash-alt"></i></button></div>
         </div>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'Templates',
+  computed: {
+    ...mapGetters({
+      languages: 'language/get',
+    }),
+  },
+  data() {
+    return {
+      tabs: ['Localization'],
+      selectedTab: 'Localization',
+      language: '',
+      slug: '',
+    };
+  },
+  methods: {
+    saveLanguage() {
+      this.$store.dispatch('language/create', { language: this.language, slug: this.slug })
+      console.log(this.language);
+    },
+    deleteLanguage(slug) {
+      this.$store.dispatch('language/delete', { slug });
+    },
+  },
 };
 </script>
 
 <style lang="less" scoped>
-
 </style>
