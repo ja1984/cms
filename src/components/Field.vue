25432cfced5ebcaf01b562677288ed3e658eab99
Andreas <andreas@fasttrack-solutions.com>
2018-09-25 19:42:06 +0200

    Fix import

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -18,7 +18,7 @@
 
 import InputField from '@/components/fields/InputField.vue';
 import TextareaField from '@/components/fields/TextareaField.vue';
-import BooleanField from '@/components/fields/BooleanField.vue';
+import BooleanField from '@/components/fields/booleanfield.vue';
 import NumberField from '@/components/fields/NumberField.vue';
 import DatePickerField from '@/components/fields/DatePickerField.vue';
 
