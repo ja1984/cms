18383607ae7650793f07463edf40d6e0efbbf0dc
Patrik Potocki <rambo3swe@gmail.com>
2018-09-26 22:43:01 +0200

    started on auth

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 
+	firebase "firebase.google.com/go"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis"
@@ -13,13 +16,31 @@ import (
 func main() {
 	log.Print("Started cms backend 😎")
 
+	var err error
+	ctx := context.Background()
+	app, err := firebase.NewApp(ctx, nil)
+
+	if err != nil {
+		fmt.Errorf("error getting Auth client: %v", err)
+	}
+
+	//Temp stuff just to get it building , need to figure out of we should do with credentails for firebase auth
+	if app != nil {
+		client, err := app.Auth(ctx)
+		if err != nil {
+			fmt.Errorf("error getting Auth client: %v", err)
+		}
+
+		log.Print(client)
+	}
+
 	database.REDIS = redis.NewClient(&redis.Options{
 		Addr:     "redis:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
 
-	err := database.REDIS.Ping().Err()
+	err = database.REDIS.Ping().Err()
 
 	if err != nil {
 		log.Fatalf("Redis errored on ping err: %v", err)
